900b760d5ab74e5b1004619798edd4710c5c7fa9
Tanvi Devi <65415114+Tanvii17@users.noreply.github.com>
2023-02-13 19:13:12 +0530

    Delete src directory

--- src/main/java/com/employeeManagement/Repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.employeeManagement.Repository;
-
-import com.employeeManagement.entity.Employee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee,Integer> {
-}
